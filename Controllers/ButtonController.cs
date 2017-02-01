4e798118b5bbe7cc21f0c5e49ac373690b383e82
juh55rt99 <healing.a.nation@gmail.com>
2017-01-31 21:56:02 -0600

    Add files via upload

---
+++ Controllers/ButtonController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using EzyMVC.Models;
+
+namespace EzyMVC.Controllers
+{
+    public class ButtonController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("");
+        }
+
+        public JsonResult GetRates()
+        {
+            List<string> country = new List<string>() { "EUR", "USD" };
+            string xmlData = HttpContext.Server.MapPath("~/App_Data/CurrencyRates.xml");
+            DataSet ds = new DataSet();
+            ds.ReadXml(xmlData);
+            var rates = new List<Row>();
+            rates = (from rows in ds.Tables[0].AsEnumerable()
+                     where country.Contains(rows.Field<string>("swift_code"))
+                     select new Row
+                     {
+                         Swift_code = rows[0].ToString(),
+                         Swift_name = rows[1].ToString(),
+                         Multiply = rows[2].ToString(),
+                         Buy_cash = rows[3].ToString(),
+                         Buy_tc = rows[4].ToString(),
+                         Sell_cash = rows[5].ToString(),
+                         Sell_tc = rows[6].ToString(),
+                         CurrencyGuide = rows[7].ToString(),
+                         Base_swift = rows[8].ToString()
+                     }).ToList();
+            ds.Dispose();
+
+            return Json(rates, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Models/CurrencyRates.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+
+namespace EzyMVC.Models
+{
+    public class CurrencyRates
+    {
+
+    }
+
+    [XmlRoot(ElementName = "timestamp")]
+    public class Timestamp
+    {
+        [XmlAttribute(AttributeName = "Desc")]
+        public string Desc { get; set; }
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "row")]
+    public class Row
+    {
+        [XmlElement(ElementName = "swift_code")]
+        public string Swift_code { get; set; }
+        [XmlElement(ElementName = "swift_name")]
+        public string Swift_name { get; set; }
+        [XmlElement(ElementName = "multiply")]
+        public string Multiply { get; set; }
+        [XmlElement(ElementName = "buy_cash")]
+        public string Buy_cash { get; set; }
+        [XmlElement(ElementName = "buy_tc")]
+        public string Buy_tc { get; set; }
+        [XmlElement(ElementName = "sell_cash")]
+        public string Sell_cash { get; set; }
+        [XmlElement(ElementName = "sell_tc")]
+        public string Sell_tc { get; set; }
+        [XmlElement(ElementName = "CurrencyGuide")]
+        public string CurrencyGuide { get; set; }
+        [XmlElement(ElementName = "base_swift")]
+        public string Base_swift { get; set; }
+    }
+
+    [XmlRoot(ElementName = "web_dis_rates")]
+    public class Web_dis_rates
+    {
+        [XmlElement(ElementName = "timestamp")]
+        public Timestamp Timestamp { get; set; }
+        [XmlElement(ElementName = "row")]
+        public List<Row> Row { get; set; }
+        [XmlElement(ElementName = "copyright")]
+        public string Copyright { get; set; }
+    }
+
+}
